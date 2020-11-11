bfb5394955b90d40f54249d501d0527404633704
liaowenqiang <wenqiang.liao@liaowenqiangdeMacBook-Pro.local>
2020-11-11 17:52:17 +0800

    [Wenqiang Liao] feat: add hlight when router switch

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,31 @@
 import CssModules from 'react-css-modules';
 import styles from './footer.module.scss';
-import { Button } from "antd";
 import { UndoOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
 
 function Footer() {
+
   return (
     <div className={styles.footer}>
-      <Button>
-        <NavLink to='/'>
-            全部
-        </NavLink>
-      </Button>
-      <Button>
-        <NavLink to='/completed'>
-            已完成
-        </NavLink>
-      </Button>
-      <Button>
-        <NavLink to='/unCompleted'>
-            未完成
-        </NavLink>
-      </Button>
+      <NavLink 
+        to='/'
+        exact
+        activeClassName={styles.active}
+      >
+        全部
+      </NavLink>
+      <NavLink 
+        to='/completed'
+        activeClassName={styles.active}
+      >
+        已完成
+      </NavLink>
+      <NavLink 
+        to='/unCompleted'
+        activeClassName={styles.active}
+      >
+        未完成
+      </NavLink>
       <UndoOutlined />
     </div>
   ); 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './main.css';
 import { connect } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import AllEvents from './allEvents/AllEvents';
 import CompletedEvents from './completedEvent/CompletedEvents';
 import UncompletedEvents from './uncompletedEvents/UncompletedEvents';
@@ -11,9 +11,10 @@ class Main extends Component {
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={AllEvents} />
           <Route path='/completed' component={CompletedEvents} />
           <Route path='/unCompleted' component={UncompletedEvents} />
+          <Route exact path='/' component={AllEvents} />
+          <Redirect to='/' />
         </Switch>
       </div>
     );
